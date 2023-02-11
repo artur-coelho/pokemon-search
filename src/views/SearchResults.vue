6b1f430903681c142123c9dba3eb365454494c83
Artur Coelho <artur.rocha.coelho@gmail.com>
2023-02-11 13:44:30 -0300

    feat: added not found page

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -1,10 +1,14 @@
 <template>
   <v-container class="fill-height">
     <v-responsive>
-      <v-row class="ma-0 mt-10">
+      <v-row v-if="pokemonsResult.length" class="ma-0 mt-10">
         <v-col col="12" sm="6" md="4" v-for="pokemon in pokemonsResult" :key="pokemon.id">
           <PokemonCard :pokemon="pokemon" />
         </v-col>
+      </v-row>
+      <v-row v-else class="ma-0 mt-10 align-center justify-center">
+        <h3>No pokémon found</h3>
+        <v-btn to="/" class="ma-4">Back to Home</v-btn>
       </v-row>
     </v-responsive>
   </v-container>
@@ -20,5 +24,4 @@ import PokemonCard from '@/components/pokemon/PokemonCard.vue';
 const store = useAppStore()
 
 const pokemonsResult = computed(() => store.currentPokemonsGetter);
-
 </script>
