773ceb11547f6de1efa623a76b33f2ad5386b696
Artur Coelho <artur.rocha.coelho@gmail.com>
2023-02-11 11:27:23 -0300

    feat: created results page

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: 'search-results',
+        name: 'SearchResults',
+        component: () => import('@/views/SearchResults.vue')
+      }
     ],
   },
 ]
---
+++ src/views/SearchResults.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive>
+      <v-row>
+        <v-col sm="4" v-for="pokemon in pokemonsResult" :key="pokemon.id">
+          <PokemonCard :pokemon="pokemon" />
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useAppStore } from '@/store/app'
+
+import PokemonCard from '@/components/pokemon/PokemonCard.vue';
+
+const store = useAppStore()
+
+const pokemonsResult = computed(() => store.currentPokemonsGetter);
+
+</script>
